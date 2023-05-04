c19c0b1b0d1c162ddcf8dd99643720cf897244f2
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-05-04 23:25:24 +0200

    styled button

--- vite-project/src/pages/components/button/index.jsx
+++ vite-project/src/pages/components/button/index.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 import { useState } from 'react';
+const Button = styled.button`
+  background-color: rgb(21, 25, 24);
+  color: rgb(182, 255, 175);
+  border: none;
+  border-radius: 5px;
+  padding: 10px 15px;
+  font-size: 1.4rem;
+`;
+const Background = styled.div`
+  background: linear-gradient(to right, rgb(21, 25, 24), rgb(182, 255, 175));
+  border-radius: 5px;
+  padding: 1px;
+  width: fit-content;
+`;
 
-const [count, setCount] = useState(0)
-export function Button() {
+export function Buttons() {
+    const [count, setCount] = useState(0);
     return(
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        //<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <Background>
+            <Button onClick={() => setCount((count) => count + 1)}>{"Clicked: " + count + " times"}</Button>
+        </Background>
+        
     )
 }
--- vite-project/src/pages/home/index.jsx
+++ vite-project/src/pages/home/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import styles from "./style.module.css"
+import styles from "./style.module.css";
+import { Buttons } from "../components/button";
 
 export function HomePage() {
     return(
         <div className = {styles.wrapper}>
-            <div className = {styles.mainContent}>
+            <span className = {styles.mainContent}>
               <h1>Welcome to my React App !</h1>
-            </div>
+              <Buttons>GO</Buttons>
+            </span>
         </div>
     )
 }
