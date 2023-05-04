1bb635e53d871e109985f8e6a3729fb552d36424
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-05-04 09:07:54 +0200

    addfs fontawsome

--- vite-project/src/pages/components/navbar/index.jsx
+++ vite-project/src/pages/components/navbar/index.jsx
@@ -3,8 +3,12 @@ import styled from "styled-components";
 //import "./navstyle.css";
 
 const Nav =  styled.nav`
-  border: solid 1px rgb(158, 250, 163);
+  border-bottom: solid 1px rgb(158, 250, 163);
   color: rgb(109, 206, 100);
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
 `;
 const Li = styled.li`
   padding: 5px;
@@ -13,12 +17,18 @@ const Li = styled.li`
 const Ul = styled.ul`
   display: flex;
   list-style-type: none;
+  padding: 5px;
+`;
+const I = styled.i`
+  font-size: 2.5rem;
+  padding: 5px;
 `;
 
 console.log(Nav)
 export function NavBar() {
     return(
         <Nav>
+          <I className="icon fa-brands fa-react"></I>
             <Ul>
                 <Li isActive = {true}>Home</Li>
                 <Li>Page 1</Li>
