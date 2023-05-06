975bfad13a2fa16ebaffc25720904e23c159db72
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-05-06 22:42:39 +0200

    adds lorem text

--- vite-project/src/pages/home/index.jsx
+++ vite-project/src/pages/home/index.jsx
@@ -7,7 +7,12 @@ export function HomePage() {
         <div className = {styles.wrapper}>
             <span className = {styles.mainContent}>
               <h1>Welcome to my React App !</h1>
-              <Buttons>GO</Buttons>
+              <Buttons></Buttons>
+              <p>
+                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. In dignissimos vero 
+                 molestias voluptate tempora! Hic voluptas maxime explicabo. Aspernatur officiis laborum nesciunt saepe 
+                 accusantium praesentium repudiandae modi sunt repellat quisquam.
+              </p>
             </span>
         </div>
     )
